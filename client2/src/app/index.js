f2b8ee30357b3cedfd71afdb64278536f42e3096
juantoman <juan.torres@iestacio.com>
2022-03-16 14:12:49 +0100

    client2 rq

--- client2/src/app/index.js
+++ client2/src/app/index.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { QueryClient, QueryClientProvider} from 'react-query'
 
 import { NavBar } from '../components'
+import { MoviesList, MoviesInsert, MoviesUpdate } from '../pages'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+const queryClient = new QueryClient()
+
 function App() {
     return (
-        <Router>
-            <NavBar />
-        </Router>
+      <Router>
+        <QueryClientProvider client={queryClient}>
+          <NavBar />
+          <Routes>
+              <Route path="/movies/list" element={<MoviesList/>} />
+              <Route path="/movies/create" exact element={<MoviesInsert/>} />
+              <Route path="/movies/update/:id" exact element={<MoviesUpdate/>} />
+          </Routes>
+        </QueryClientProvider>
+      </Router>
     )
 }
 
