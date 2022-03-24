223a2c30ff395332d34391bcdfd9ad10dc9a4a24
juantoman <juan.torres@iestacio.com>
2022-03-24 20:40:23 +0100

    mysymbaloo 0.1

--- client2/src/pages/MoviesList.jsx
+++ client2/src/pages/MoviesList.jsx
@@ -16,6 +16,8 @@ import CardHeader from '@mui/material/CardHeader';
 import IconButton, { IconButtonProps } from '@mui/material/IconButton';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Paper from '@mui/material/Paper';
+import BorderRadius from '../components/BorderRadius';
+import Avatar from '@mui/material/Avatar';
 
 const DeleteMovie = props => {
 
@@ -57,7 +59,7 @@ const MoviesList = () => {
 
   return (
     <div>
-      
+
       {/*<table class="table" style={{marginTop:'100px'}}>
         <thead>
           <tr>
@@ -91,7 +93,7 @@ const MoviesList = () => {
         </tbody>
       </table>*/}
       <Container maxWidth="false">
-        <Box sx={{ flexGrow: 1 , m: 1 }}>
+        {/*<Box sx={{ flexGrow: 1 , m: 1 }}>
           <Grid container direction="row" justifyContent="center" alignItems="center" spacing={2}>
             {
               data.data.data.map((movie, index) => {
@@ -106,7 +108,9 @@ const MoviesList = () => {
                           height="140"
                           image="https://i.blogs.es/89c7de/uncharted-cartel/1366_2000.jpeg"
                           alt="UNCHARTED"
-                        />
+                        /><IconButton aria-label="settings">
+                          <MoreVertIcon />
+                        </IconButton>
                         <CardContent>
                           <Typography gutterBottom variant="h5" component="div">
                             {movie.name}
@@ -151,6 +155,27 @@ const MoviesList = () => {
               )
             })
           }
+        </Box>*/}
+        <Box sx={{ flexGrow: 1 , m: 1 }}>
+          <Grid container direction="row" justifyContent="center" alignItems="center" spacing={1}>
+            <Link style={{textDecoration:"none"}} to="/movies/create">
+              <Box sx={{ border: 1, borderRadius: '5px' , m: "10px", p: "20px"}}>
+                <Avatar>+</Avatar>
+              </Box>
+            </Link>
+            {
+              data.data.data.map((movie, index) => {
+                let a="https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=" + movie.name +"&size=64"
+                return (
+                  <a href={movie.name} target="_blank">
+                    <Box sx={{ border: 1, borderRadius: '5px' , m: "10px", p: "20px"}}>
+                      <Avatar variant="square" src={a}>{movie.name}</Avatar>
+                    </Box>
+                  </a>
+                )
+              })
+            }
+          </Grid>
         </Box>
       </Container>
 
