08af518458d69f2e2158f08e8a45b7eef5384f2f
juantoman <juantoman@professor-aula22.foremanb.iestacio.com>
2022-03-31 14:16:13 +0200

    mysymbaloo 0.10

--- client2/src/app/index.js
+++ client2/src/app/index.js
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider} from 'react-query'
 
 import { ResponsiveAppBar } from '../components'
 import { MoviesList, MoviesInsert, MoviesUpdate } from '../pages/Pages'
+import { GoogleLogin, GoogleLogout } from 'react-google-login'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -22,11 +23,35 @@ const queryClient = new QueryClient({
   },
 });
 
+const onSuccessLogin = response => {
+  alert(response.profileObj.email);
+};
+
+const onLogoutSuccess = response => {
+  alert("Logout!!!");
+};
+
+const onFailure = response => {
+  alert("Error!!!");
+};
+
 function App() {
     return (
       <Router>
         <QueryClientProvider client={queryClient}>
           <ResponsiveAppBar/>
+          <GoogleLogin
+            clientId="422269930750-src3psqemmt1p6m8alujf9nvmook5c0d.apps.googleusercontent.com"
+            buttonText="Login with Google"
+            onSuccess={onSuccessLogin}
+            onFailure={onFailure}
+            cookiePolicy="single_host_origin"
+          />
+          <GoogleLogout
+            clientId="422269930750-src3psqemmt1p6m8alujf9nvmook5c0d.apps.googleusercontent.com"
+            buttonText="Logout with Google"
+            onLogoutSuccess={onLogoutSuccess}
+          />
           <Routes>
               <Route path="/movies/list" exact element={<MoviesList/>} />
               <Route path="/movies/create" exact element={<MoviesInsert/>} />
