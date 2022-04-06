e754966dc0bfb0e6e03e8a6acbf7ae8cf0318507
juantoman <juan.torres@iestacio.com>
2022-04-06 08:29:04 +0200

    mysymbaloo 0.13

--- client2/src/pages/Presentation.jsx
+++ client2/src/pages/Presentation.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
+const root = {
+    position: 'absolute',
+    background: "linear-gradient(45deg, #5555ff 15%, #aaaaaa 90%)",
+    minWidth: "100%",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+    top:0
+}
+
 const Presentation = () => {
+
     return (
-        <h1>MySymbaloo</h1>
+        <h1 style={root}>MySymbaloo</h1>
     )
 }
 
