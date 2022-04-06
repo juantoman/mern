8047cb4fedd05cb65a5631feefee234dbefc2f3b
juantoman <juan.torres@iestacio.com>
2022-04-06 17:26:53 +0200

    mysymbaloo 0.16

--- client2/src/app/index.js
+++ client2/src/app/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
 import { QueryClient, QueryClientProvider} from 'react-query'
 
 import { ResponsiveAppBar } from '../components'
@@ -30,12 +30,33 @@ function App() {
       isAuthenticated: false,
       email: ''
     }
-    const [context, setContext] = React.useState(userInfo);
-    //console.log({context})
+
+    if ( !localStorage.getItem("userInfo") ) {
+      localStorage.setItem('userInfo', JSON.stringify(userInfo))
+    }
+
+    const [context, setContext] = React.useState(JSON.parse(localStorage.getItem("userInfo")));
+    //console.log(context.isAuthenticated)
+
+    if ( !localStorage.getItem("userInfo") ) {
+      localStorage.setItem('userInfo', JSON.stringify(userInfo))
+    }
 
     React.useEffect(() => {
       setContext(JSON.parse(localStorage.getItem("userInfo")))
     },[]);
+
+    function PrivateRoute({ children }) {
+      return context.isAuthenticated ? children : <Navigate to="/" />;
+    }
+
+    function PrivateRoute({ children }) {
+      return context.isAuthenticated ? children : <Navigate to="/" />;
+    }
+
+    function UserNotAuthentified({ children }) {
+      return context.isAuthenticated ? <Navigate to="/movies/list" /> : children;
+    }
 
     return (
       <Router>
@@ -44,10 +65,10 @@ function App() {
             <ResponsiveAppBar/>
             <h3>{context.email}</h3>
             <Routes>
-              <Route path="/movies/list" exact element={<MoviesList/>} />
-              <Route path="/movies/create" exact element={<MoviesInsert/>} />
-              <Route path="/movies/update/:id" exact element={<MoviesUpdate/>} />
-              <Route path="/" exact element={<Presentation/>} />
+              <Route path="/movies/list" exact element={<PrivateRoute><MoviesList/></PrivateRoute>} />
+              <Route path="/movies/create" exact element={<PrivateRoute><MoviesInsert/></PrivateRoute>} />
+              <Route path="/movies/update/:id" exact element={<PrivateRoute><MoviesUpdate/></PrivateRoute>} />
+              <Route path="/" exact element={<UserNotAuthentified><Presentation/></UserNotAuthentified>} />
             </Routes>
           </Context.Provider>
         </QueryClientProvider>
