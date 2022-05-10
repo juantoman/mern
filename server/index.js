c038b8c925ba9644f4e77a7aef37c13ffcbd2db8
juantoman <juan.torres@iestacio.com>
2022-05-10 17:19:16 +0200

    https14

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const cors = require('cors')
 const db = require('./db')
 const movieRouter = require('./routes/movie-router')
 var router = express.Router();
-const https = require('https')
 
 const app = express()
 const apiPort = 3000
@@ -22,9 +21,7 @@ app.get('/', (req, res) => {
 
 app.use('/api', movieRouter)
 
-https.createServer(app).listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
-
-//app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
 
 // will redirect all the non-api routes to react frontend
 router.use(function(req, res) {
