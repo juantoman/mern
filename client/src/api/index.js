392d29b5cebb7f67d1ff77175b83ae1668244c6d
juantoman <juan.torres@iestacio.com>
2022-05-12 16:49:02 +0200

    https24

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: `https://api.iestacio.com/api`,
+    baseURL: `https://api.mysymbaloo.tk/api`,
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
