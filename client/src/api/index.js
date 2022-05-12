f2d546cf0ab09c8ce0033a265733fb67c1dfe1c7
juantoman <juan.torres@iestacio.com>
2022-05-12 06:46:36 +0200

    https22

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: `https://localhost:3000/api`,
+    baseURL: `https://api.iestacio.com/api`,
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
