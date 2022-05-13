5f9c0ec228966acbee70fa4aaf9b297e9d0b94cc
juantoman <juan.torres@iestacio.com>
2022-05-13 07:05:58 +0200

    https_symbi.ga

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: `https://api.cocheco.ga/api`,
+    baseURL: `https://api.symbi.ga/api`,
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
